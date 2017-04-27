cd8c15627371ab75c154f3510d605004aa8f5c57
William Liu <william.q.liu@gmail.com>
2017-04-27 10:51:01 -0600

    Set log level info for celery to help debug

--- web/scripts/post-run-celery.sh
+++ web/scripts/post-run-celery.sh
@@ -2,5 +2,5 @@
 echo "Running celery worker script!";
 sleep 30  # wait for other servers to start up
 #pip install -r /web/requirements.txt
-#celery -A hipflask.celery worker  # start up celery worker
+#celery -A hipflask.celery worker --loglevel=info  # start up celery worker
 tail -f /dev/null  # keep container up
