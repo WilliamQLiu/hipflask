c3989700a7a1df9805fe389bc7eb7a939fccd386
William Liu <william.q.liu@gmail.com>
2017-04-28 23:44:25 -0600

    Tests and data transform logic for 'companies' url

--- web/hipflask/hipflask.py
+++ web/hipflask/hipflask.py
@@ -1,12 +1,26 @@
+"""
+A sample Flask app
+Usually models, apis, routes, are split to different dirs.
+
+To run locally (without Docker):
+$export FLASK_APP = hipflask.py
+$flask run --no-reload
+"""
 from __future__ import absolute_import
 import os
 from datetime import datetime
 
-from flask import Flask, request, render_template, jsonify
+from flask import (
+    Flask, request, Response, render_template, jsonify, redirect, flash, url_for,
+    make_response
+)
 from flask_restful import Resource, Api, reqparse
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
 from redis import Redis
 from celery import Celery
+import pandas
+import numpy
 
 
 class BaseConfig(object):
@@ -21,13 +35,13 @@ class BaseConfig(object):
     SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(
         DB_USER, DB_PASSWORD, DB_SERVICE, DB_PORT, DB_NAME
     )
+    UPLOAD_FOLDER = "/uploads/"
+    ALLOWED_EXTENSIONS = set(["csv"])
 
 # Initial Setup
 application = Flask(__name__)
 application.config.from_object(BaseConfig)
 db = SQLAlchemy(application)  # for database connection
-#db.drop_all()  # Drop all existing database tables
-#db.create_all()  # Create database and database tables
 api = Api(application)  # for restful api
 
 
@@ -113,9 +127,9 @@ class BuzzList(Resource):
     def get(self):
         """
         GET list of buzz items
-        :argument 'urltext' (type str) checks if url contains this text (default None)
-        :argument 'limit' (type int) is items returned (default None, returns all) 
-        :returns list of JSON buzz items
+        :param urltext: (type str) checks if url contains this text (default None)
+        :param limit: (type int) is items returned (default None, returns all) 
+        :return list of JSON buzz items
         """
         parser = reqparse.RequestParser()
         parser.add_argument("urltext", type=str, help="URL contains text")
@@ -142,14 +156,13 @@ class BuzzList(Resource):
         return "TODO: POST"
 
 
-api.add_resource(BuzzItem, '/buzz/<int:id>/')
-api.add_resource(BuzzList, '/buzz/')
-
+api.add_resource(BuzzItem, "/buzz/<int:id>/")
+api.add_resource(BuzzList, "/buzz/")
 
 # Routes
 @application.route("/")
 def hello_world():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 @application.route("/buzz/simple/", methods=["GET", "POST"])
@@ -160,6 +173,139 @@ def buzz_norest():
         return jsonify({"counter": counter})
     buzzing = Buzz.query.limit(3)
     return render_template('buzz.html', buzzing=buzzing)
+
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
+def transform_companies_data(request, data):
+    """
+    :param request: the request
+    :param data: dict containing error, message, and dfs
+    :return: data: dict containing error, message, and merged dataframe
+    """
+    try:
+        # get dataframes of uploaded files
+        df_companies = data["dfs"]["companies"]
+        df_daily = data["dfs"]["daily"]
+
+        # merge and change data types; can do additional data validation here
+        df_merged = df_daily.merge(df_companies, on="id", how="inner")
+        df_merged["date"] = pandas.to_datetime(df_merged["date"])
+        df_merged["value"] = df_merged["value"].astype(int)
+
+        # filter for start_date and end_dates
+        start_date = request.form.get("start_date")
+        if start_date == "":
+            start_date = None
+        else:
+            start_date = datetime.strptime(start_date, "%Y-%m-%d")
+        end_date = request.form.get("end_date")
+        if end_date == "":
+            end_date = None
+        else:
+            end_date = datetime.strptime(end_date, "%Y-%m-%d")
+        if(start_date and end_date):
+            pass
+        elif(start_date):
+            end_date = df_merged.date.max()
+        elif(end_date):
+            start_date = df_merged.date.min()
+        else:
+            start_date = df_merged.date.min()
+            end_date = df_merged.date.max()
+        # Should check more data validation (e.g. start_date > end_date)
+        date_range_index = pandas.date_range(start_date, end_date)
+
+        # multilevel index by date with forward fill for missing values
+        df_grouped = df_merged.groupby(["date", "id"]).sum()
+        (date_index, id_index) = df_grouped.index.levels
+        new_index = pandas.MultiIndex.from_product([date_range_index, id_index])
+        new_df = df_grouped.reindex(new_index)
+        new_df = new_df.reset_index()
+        new_df.columns = ["date", "id", "value"]
+
+        # Get difference (last n rows) based on grouping by company "id"
+        n_difference = request.form.get("n")
+        if(is_number(n_difference)):
+            pass
+        else:
+            n_difference = 1
+        diff = lambda x: x["value"].replace(0, numpy.NaN).dropna().diff(n_difference)
+        df_grouped = new_df.groupby("id")
+        new_df["difference"] = df_grouped.apply(diff).reset_index(0, drop=True)
+    except Exception as e:
+        data["error"] = True
+        data["message"] = str(e)
+        data["dfs"]["merged"] = None
+        return data
+
+    data["error"] = False
+    data["message"] = "Successfully merged files"
+    data["dfs"]["merged"] = new_df
+    return data
+
+
+def validate_companies_file_upload(request):
+    """
+    Validate file uploads 
+    :param request: the request
+    :return data: dict that contains the following keys
+        error: boolean (default False)
+        message: string of error or success message (default None)
+        dfs: dict with keys holding dataframes
+    """
+    data = {
+        "error": False, "message": None,
+        "dfs": {"daily": None, "companies": None}
+    }
+
+    # check if files are there and correct file type
+    companies = request.files.get("companies", None)
+    daily = request.files.get("daily", None)
+    if not(companies and daily):
+        data["error"] = True
+        data["message"]= "Missing file(s): must be named 'companies.csv' and 'daily.csv'"
+    elif (companies.content_type != "text/csv" or daily.content_type != "text/csv"):
+        data["error"] = True
+        data["message"] = "File type mismatch: check that file types "
+    else:
+        data["dfs"]["daily"] = pandas.read_csv(daily)
+        data["dfs"]["companies"] = pandas.read_csv(companies)
+    return data
+
+
+@application.route("/companies/", methods=["GET", "POST"])
+def transform_daily_company_files():
+    """
+    Merge two files (daily.csv, companies.csv) based on id, 
+    then return new cleaned data file (export.csv)
+    """
+    if request.method == "POST":
+        result = validate_companies_file_upload(request)
+        if(result["error"]):
+            flash(result["message"])
+            return redirect(url_for("transform_daily_company_files"))
+
+        result = transform_companies_data(request, result)
+        if(result["error"]):
+            flash(result["message"])
+            return redirect(url_for("transform_daily_company_files"))
+
+        # send csv file if data was merged successfully
+        csv_file = result["dfs"]["merged"].to_csv(index=False)
+        response = make_response(csv_file)
+        response.headers["Content-Disposition"] = "attachment; filename=export.csv"
+        response.headers["Content-Type"] = "text/csv"
+        response.status = "200 OK"
+        response.status_code = 200
+        return response
+    return render_template("transform.html")
 
 
 if __name__ == "__main__":
--- web/hipflask/tests/test_hipflask.py
+++ web/hipflask/tests/test_hipflask.py
@@ -3,14 +3,19 @@ Example use: run the below unit tests from parent directory with
 $python -m unittest discover tests
 """
 from __future__ import absolute_import
+from io import BytesIO
+import datetime
+
 import unittest
-
 import json
+import pandas
 
-from hipflask import application
+from hipflask import (
+    application, transform_companies_data
+)
 
 
-class APIBasicTestCase(unittest.TestCase):
+class APIBasicTest(unittest.TestCase):
 
     def setUp(self):
         self.app = application
@@ -74,6 +79,78 @@ class APIBasicTestCase(unittest.TestCase):
         response = self.client.post("/buzz/simple/", data=dict(foo="bar"))
         json_data = json.loads(response.data)
         self.assertGreater(json_data["counter"], 1)
+
+
+class Request():
+    """ For fake request """
+    def __init__(self):
+        self.form = {}
+
+
+class CompaniesTest(unittest.TestCase):
+    """ Test daily companies """
+    def setUp(self):
+        self.app = application
+        self.client = self.app.test_client()
+        self.client.testing = True
+
+
+    def tearDown(self):
+        del self.app
+        del self.client
+
+    def test_get_transform_daily_company_files_route(self):
+        response = self.client.get("/companies/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_transform_file_upload_with_no_files_reroutes_correctly(self):
+        response = self.client.post(
+            "/companies/", content_type="multipart/form-data", data={}
+        )
+        self.assertEqual(response.status_code, 302)
+
+    def test_validate_file_upload_one_file_redirects(self):
+        response = self.client.post(
+            "/companies/", data = {
+                "file": (BytesIO("stuff in file"), "daily.csv"),
+            }
+        )
+        self.assertEqual(response.status_code, 302)
+
+    def test_transform_companies_data_success(self):
+        mock_companies_data = pandas.read_csv("static/test_files/companies.csv")
+        mock_daily_data = pandas.read_csv("static/test_files/daily.csv")
+        mock_data = {
+            "error": False,
+            "message": None,
+            "dfs": {
+                "companies": mock_companies_data,
+                "daily": mock_daily_data
+            }
+        }
+        mock_form = {
+            "start_date": "",
+            "end_date": "",
+            "n":""
+        }
+        fake_request = Request()
+        fake_request.form = mock_form
+        response = transform_companies_data(fake_request, mock_data)
+        self.assertIsNotNone(response["dfs"]["merged"])
+        self.assertEqual(len(response["dfs"]["merged"]), 30) # all records
+
+        #filtering by start and end date works
+        mock_form = {
+            "start_date": "2017-1-3",
+            "end_date": "2017-1-8",
+            "n":""
+        }
+        fake_request.form = mock_form
+        response = transform_companies_data(fake_request, mock_data)
+        self.assertEqual(min(response["dfs"]["merged"]["date"]),
+                         datetime.datetime(2017, 1, 3))
+        self.assertEqual(max(response["dfs"]["merged"]["date"]),
+                         datetime.datetime(2017, 1, 8))
 
 
 if __name__ == "__main__":
